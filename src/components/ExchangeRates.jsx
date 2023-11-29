031a4cee1c2d577058d9772a25d8d03e7a76c723
SebastianLozanoValle <90271134+SebastianLozanoValle@users.noreply.github.com>
2023-11-29 15:53:03 -0500

    prueba produccion

--- src/components/ExchangeRates.jsx
+++ src/components/ExchangeRates.jsx
@@ -6,13 +6,15 @@ import { Box, Center, Container, Flex, Heading, Input, MenuItemOption, Select, T
 
 // Constantes temporales
 const source = 'COP';
-const exchangeRates2 = {
-    "COPEUR": 0.000229,
-    "COPUSD": 0.000251,
-    "COPJPY": 0.03708,
-    "COPGBP": 0.000198,
-    "COPCNY": 0.001782
-};
+// const exchangeRates2 = {
+//     "COPEUR": 0.000229,
+//     "COPUSD": 0.000251,
+//     "COPJPY": 0.03708,
+//     "COPGBP": 0.000198,
+//     "COPCNY": 0.001782
+// };
+
+// const timestamp2 = 1701276063
 
 const CurrencyConverter = () => {
     const [exchangeRates, setExchangeRates] = useState({});
@@ -24,63 +26,64 @@ const CurrencyConverter = () => {
     const [lastUpdate, setLastUpdate] = useState(null);
 
     //
-    useEffect(()=>{
-        setExchangeRates(exchangeRates2)
-    },[])
+    // useEffect(() => {
+    //     setExchangeRates(exchangeRates2)
+    //     // setLastUpdate(timestamp2)
+    // }, [])
     //
 
-    // useEffect(() => {
-    //     // Verificar si es la primera vez que se abre la página
-    //     const isFirstTime = localStorage.getItem('isFirstTime') === null;
+    useEffect(() => {
+        // Verificar si es la primera vez que se abre la página
+        const isFirstTime = localStorage.getItem('isFirstTime') === null;
 
-    //     // Si es la primera vez o han pasado más de 24 horas desde la última actualización, realizar la solicitud a la API
-    //     const lastUpdateTimestamp = localStorage.getItem('lastUpdate');
-    //     const twentyFourHoursInMilliseconds = 24 * 60 * 60 * 1000;
+        // Si es la primera vez o han pasado más de 24 horas desde la última actualización, realizar la solicitud a la API
+        const lastUpdateTimestamp = localStorage.getItem('lastUpdate');
+        const twentyFourHoursInMilliseconds = 24 * 60 * 60 * 1000;
 
-    //     if (isFirstTime || !lastUpdateTimestamp || Date.now() - Number(lastUpdateTimestamp) > twentyFourHoursInMilliseconds) {
-    //         const accessKey = '063823e4f7700fff0c4df92e4b774711';
-    //         const currencies = 'EUR,USD,JPY,GBP,CNY';
-    //         const format = '1';
+        if (isFirstTime || !lastUpdateTimestamp || Date.now() - Number(lastUpdateTimestamp) > twentyFourHoursInMilliseconds) {
+            const accessKey = '063823e4f7700fff0c4df92e4b774711';
+            const currencies = 'EUR,USD,JPY,GBP,CNY';
+            const format = '1';
 
-    //         axios.get(`http://apilayer.net/api/live?access_key=${accessKey}&currencies=${currencies}&source=${source}&format=${format}`)
-    //             .then(response => {
-    //                 // Log de la respuesta de la API
-    //                 console.log('Response from API:', response.data);
+            axios.get(`http://apilayer.net/api/live?access_key=${accessKey}&currencies=${currencies}&source=${source}&format=${format}`)
+                .then(response => {
+                    // Log de la respuesta de la API
+                    console.log('Response from API:', response.data);
 
-    //                 // Almacenar los datos de la respuesta en el estado
-    //                 setExchangeRates(response.data.quotes);
+                    // Almacenar los datos de la respuesta en el estado
+                    setExchangeRates(response.data.quotes);
 
-    //                 // Almacenar los datos de la respuesta en el localStorage
-    //                 localStorage.setItem('exchangeRates', JSON.stringify(response.data.quotes));
+                    // Almacenar los datos de la respuesta en el localStorage
+                    localStorage.setItem('exchangeRates', JSON.stringify(response.data.quotes));
 
-    //                 // Actualizar el timestamp en localStorage
-    //                 const currentTimestamp = Date.now();
-    //                 localStorage.setItem('lastUpdate', currentTimestamp);
-    //                 setLastUpdate(currentTimestamp);
+                    // Actualizar el timestamp en localStorage
+                    const currentTimestamp = Date.now();
+                    localStorage.setItem('lastUpdate', currentTimestamp);
+                    setLastUpdate(currentTimestamp);
 
-    //                 // Marcar que ya se ha realizado la solicitud
-    //                 localStorage.setItem('isFirstTime', 'false');
-    //             })
-    //             .catch(error => {
-    //                 console.error('Error fetching exchange rates:', error);
-    //             });
-    //     } else {
-    //         // Si no es la primera vez y no ha pasado más de 24 horas, obtener los datos del localStorage
-    //         const storedExchangeRates = localStorage.getItem('exchangeRates');
-    //         if (storedExchangeRates) {
-    //             setExchangeRates(JSON.parse(storedExchangeRates));
-    //         }
+                    // Marcar que ya se ha realizado la solicitud
+                    localStorage.setItem('isFirstTime', 'false');
+                })
+                .catch(error => {
+                    console.error('Error fetching exchange rates:', error);
+                });
+        } else {
+            // Si no es la primera vez y no ha pasado más de 24 horas, obtener los datos del localStorage
+            const storedExchangeRates = localStorage.getItem('exchangeRates');
+            if (storedExchangeRates) {
+                setExchangeRates(JSON.parse(storedExchangeRates));
+            }
 
-    //         setLastUpdate(Number(lastUpdateTimestamp));
-    //     }
-    // }, []);
+            setLastUpdate(Number(lastUpdateTimestamp));
+        }
+    }, []);
 
     useEffect(() => {
         // Calcular el monto convertido cuando cambia la cantidad o las tasas de cambio
         if (exchangeRates && amountCOP) {
             // Asegúrate de usar el formato correcto cuando accedes a las tasas de cambio
             const converted = amountCOP * exchangeRates[`COP${selectedCurrency}`];
-            setAmountSelectedCurrency(converted.toFixed(5));
+            setAmountSelectedCurrency(converted.toFixed(2));
         }
     }, [exchangeRates, amountCOP, selectedCurrency]);
 
@@ -89,7 +92,7 @@ const CurrencyConverter = () => {
         if (exchangeRates && amountUSD) {
             // Asegúrate de usar el formato correcto cuando accedes a las tasas de cambio
             const converted = amountUSD / exchangeRates[`COP${selectedCurrency}`];
-            setAmountSelectedCurrencyUSD(converted.toFixed(5));
+            setAmountSelectedCurrencyUSD(converted.toFixed(2));
         }
     }, [exchangeRates, amountUSD, selectedCurrency]);
 
@@ -100,11 +103,11 @@ const CurrencyConverter = () => {
 
         // Calcular el monto convertido en la otra dirección
         const convertedUSD = newAmountCOP * exchangeRates[`COP${selectedCurrency}`];
-        setAmountUSD(convertedUSD.toFixed(5));
+        setAmountUSD(convertedUSD.toFixed(2));
 
         // Actualizar el monto en USD automáticamente
         const converted = newAmountCOP * exchangeRates[`COP${selectedCurrency}`];
-        setAmountSelectedCurrency(converted.toFixed(5));
+        setAmountSelectedCurrency(converted.toFixed(2));
     };
 
     const handleAmountUSDChange = (newAmountUSD) => {
@@ -112,11 +115,11 @@ const CurrencyConverter = () => {
 
         // Calcular el monto convertido en la otra dirección
         const convertedCOP = newAmountUSD / exchangeRates[`COP${selectedCurrency}`];
-        setAmountCOP(convertedCOP.toFixed(5));
+        setAmountCOP(convertedCOP.toFixed(2));
 
         // Actualizar el monto en COP automáticamente
         const converted = newAmountUSD / exchangeRates[`COP${selectedCurrency}`];
-        setAmountSelectedCurrencyUSD(converted.toFixed(5));
+        setAmountSelectedCurrencyUSD(converted.toFixed(2));
     };
 
     return (
@@ -127,33 +130,52 @@ const CurrencyConverter = () => {
                         <Heading m={5}>Boletín Informativo</Heading>
                     </Center>
 
-                    <Container display='flex' flexWrap='wrap' minW='90vw' minH='4px'>
+                    <Container display='flex' flexWrap='wrap' minW='90vw' minH='auto'>
                         {/* Tarjeta para el Peso Colombiano (COP) */}
-                        <Center mx='auto' my={5} minH='100%' minW='550px'>
-                            <Box flex={1} p={10} boxShadow='2xl' my={5} borderRadius={20} height='100%'>
+                        <Center ml='auto' mr={{ base: "auto", md: "auto", lg: 5 }} my={5} minH='100%' >
+                            <Box flex={1} p={10} boxShadow='2xl' my={5} borderRadius={20} width='100%' height='100%'
+                                _hover={{
+                                    transform: 'scale(1.2)',
+                                    bg: 'brand.primary',
+                                    color: '#fff'
+                                }}
+                                transition='.5s'
+                            >
                                 <Heading>Peso Colombiano (COP)</Heading>
                                 <Box>
                                     <Box>
-                                        Monto:
+                                        <Text
+                                            py={4}
+                                        >
+                                            Monto:
+                                        </Text>
                                         <Input
+                                            mb={4}
                                             type="number"
                                             value={amountCOP}
                                             onChange={(e) => handleAmountCOPChange(e.target.value)}
                                         />
                                     </Box>
                                 </Box>
-                                <Text color='brand.primary'>
+                                <Text color='brand.background'>
                                     {amountCOP} COP equivale a {amountSelectedCurrency} {selectedCurrency}.
                                 </Text>
                             </Box>
                         </Center>
 
                         {/* Tarjeta para otras Boxisas */}
-                        <Center mx='auto' my={5} minH='100%' minW='550px'>
-                            <Box flex={1} p={10} boxShadow='2xl' my={5} borderRadius={20} height='100%'>
+                        <Center mr='auto' ml={{ base: "auto", md: "auto", lg: 5 }} my={5} minH='100%' >
+                            <Box flex={1} p={10} boxShadow='2xl' my={5} borderRadius={20} width='100%' height='100%'
+                                _hover={{
+                                    transform: 'scale(1.2)',
+                                    bg: '#c9c9c9',
+                                    color: 'brand.primary'
+                                }}
+                                transition='.5s'
+                            >
                                 {/* <h2>Otras Divisas</h2> */}
                                 <label>
-                                    <Heading>Elija La Divisa:</Heading>
+                                    <Heading pb={4}>Elija La Divisa:</Heading>
                                     <Select onChange={(e) => setSelectedCurrency(e.target.value)} value={selectedCurrency}>
                                         {currenciesOptions.map(currency => (
                                             <option key={currency} value={currency}>
@@ -164,7 +186,11 @@ const CurrencyConverter = () => {
                                 </label>
                                 <Box>
                                     <label>
-                                        Monto:
+                                        <Text
+                                            py={4}
+                                        >
+                                            Monto:
+                                        </Text>
                                         <Input
                                             type="number"
                                             value={amountUSD}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,89 @@
-import { Box, Container, Flex, Link as CustomLink, Text } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
-import { FaWhatsapp } from "react-icons/fa";
+// import { Box, Container, Flex, Link as CustomLink, Text } from "@chakra-ui/react";
+// import { Link } from "react-router-dom";
+// import { FaWhatsapp } from "react-icons/fa";
 
-export const Footer = () => 
+// export const Footer = () => 
+//     <footer>
+//         <Box height='50vh' mt="auto" py="8" bg="#000" color="#fff" p={9}>
+//             <Container maxW="90%" py={20}>
+//                 <Flex justify="space-between" align="center">
+//                     <Text>&copy; 2023 Tu Empresa</Text>
+//                     <Flex>
+//                     <CustomLink as={Link} mx="2" to="/terminos">
+//                         Términos de Servicio
+//                     </CustomLink>
+//                     <CustomLink as={Link} mx="2" to="/privacidad">
+//                         Política de Privacidad
+//                     </CustomLink>
+//                     </Flex>
+//                 </Flex>
+//             </Container>
+//             <a target='_blank' href="http://wa.me/573138461114" rel="nofollow noopener noreferrer" className='flotante ezquina'><FaWhatsapp className='ezquina' /></a>
+//         </Box>
+//     </footer>
+
+import React from 'react';
+import { Box, Center, Container, Flex, Img } from "@chakra-ui/react";
+import { Link as CustomLink } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { FaFacebookF } from "react-icons/fa";
+import { CiLinkedin } from "react-icons/ci";
+import { FaInstagram } from "react-icons/fa6";
+import { FaWhatsapp } from "react-icons/fa";
+import logoempresa from '../assets/img/logoempresa.png';
+
+export const Footer = () => (
     <footer>
-        <Box height='50vh' mt="auto" py="8" bg="#000" color="#fff" p={9}>
-            <Container maxW="90%" py={20}>
-                <Flex justify="space-between" align="center">
-                    <Text>&copy; 2023 Tu Empresa</Text>
-                    <Flex>
-                    <CustomLink as={Link} mx="2" to="/terminos">
-                        Términos de Servicio
-                    </CustomLink>
-                    <CustomLink as={Link} mx="2" to="/privacidad">
-                        Política de Privacidad
-                    </CustomLink>
-                    </Flex>
+        <Box w='100vw' bg="#000">
+            <Container minW='90vw'>
+                <Flex
+                    bg="#000"
+                    color="#fff"
+                    p={9}
+                    justifyContent='center'
+                    width='100%'
+                    height='100%'
+                    flexWrap='wrap' // Hace que los elementos se coloquen en columna cuando el espacio es insuficiente
+                    align='center'
+                >
+                    <Img
+                        src={logoempresa}
+                        height='auto'
+                        width='200px'
+                        flex={1}
+                    />
+                    <Container
+                        display='block'
+                        textAlign='just'
+                        margin='0px'
+                        height='100%'
+                        flex={1}
+                    >
+                        <CustomLink as={Link} to={'/'}>
+                            Inicio
+                        </CustomLink><br />
+                        <CustomLink as={Link} to={'/contacto'}>
+                            Contacto
+                        </CustomLink><br />
+                        <CustomLink as={Link} to={'/politica'}>
+                            Politica tratamiento de datos
+                        </CustomLink><br />
+                        <CustomLink as={Link} to={'/pservicios'}>
+                            Prestacion de servicios
+                        </CustomLink>
+                    </Container>
+
+                    <Box flex={1}>
+                        <Container width='auto' display='flex' gap='40px' justifyContent='center' alignItems='center'>
+                            <a href="https://www.facebook.com/mcshipbrokers/?locale=es_LA"><Box fontSize='40px'>{<FaFacebookF />}</Box></a>
+                            <a href="https://www.linkedin.com/company/2879222/admin/feed/posts/"><Box fontSize={'40px'}>{<CiLinkedin />}</Box></a>
+                            <a href="https://www.instagram.com/mcshipbrokersint/"><Box fontSize={'40px'}>{<FaInstagram />}</Box></a>
+                        </Container>
+                    </Box>
+
+                    <a target='_blank' href="http://wa.me/573138461114" rel="nofollow noopener noreferrer" className='flotante ezquina'><FaWhatsapp className='ezquina' /></a>
                 </Flex>
             </Container>
-            <a target='_blank' href="http://wa.me/573138461114" rel="nofollow noopener noreferrer" className='flotante ezquina'><FaWhatsapp className='ezquina' /></a>
         </Box>
     </footer>
+);
